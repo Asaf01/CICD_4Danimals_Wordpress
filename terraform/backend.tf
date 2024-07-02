709a2ac0e2491b6a604ac899008bc3433d78f450
Asaf01 <123545280+Asaf01@users.noreply.github.com>
2024-07-02 20:40:30 +0300

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "jenkins-terrafora-k8s"
-    region = "eu-west-3"
+    bucket = "jenkins-terra-k8s"
+    region = "us-east-1"
     key = "eks/terraform.tfstate"
   }
 }
