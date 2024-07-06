7220211ee165f1c9e7beb6263a23eda8df956bc1
Asaf01 <123545280+Asaf01@users.noreply.github.com>
2024-07-06 16:32:50 +0300

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -11,14 +11,14 @@ module "eks" {
 
     tags = {
         environment = "development"
-        application = "nginx-app"
+        application = "wordpress"
     }
 
     eks_managed_node_groups = {
         dev = {
-            min_size = 1
-            max_size = 2
-            desired_size = 1
+            min_size = 2
+            max_size = 4
+            desired_size = 2
 
             instance_types = ["t2.small"]
         }
