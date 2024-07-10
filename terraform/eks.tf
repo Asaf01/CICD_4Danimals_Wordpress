713bf695670536ea4c41b0dea48daa239b69fe54
Asaf01 <123545280+Asaf01@users.noreply.github.com>
2024-07-10 21:54:46 +0300

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -4,14 +4,11 @@ module "eks" {
     cluster_name = "my-eks-cluster"
     cluster_version = "1.24"
 
+
     cluster_endpoint_public_access  = true
 
-    vpc_id = module.app-vpc.vpc_id
-  subnet_ids = [
-    "subnet-053e675a9fd9c2166",  # us-east-1c
-    "subnet-0488f5ddafc8f2608",  # us-east-1c
-    "subnet-01cda2b848a10f409"   # us-east-1b (adding this to meet EKS requirement)
-     ]
+    vpc_id = module.my-vpc.vpc_id
+    subnet_ids = module.my-vpc.private_subnets
 
     tags = {
         environment = "development"
