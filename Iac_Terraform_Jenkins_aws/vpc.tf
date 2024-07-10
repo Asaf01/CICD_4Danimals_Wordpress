be69bf055414888619f953881ad786825aa168db
Asaf01 <123545280+Asaf01@users.noreply.github.com>
2024-07-10 15:30:58 +0300

    Update vpc.tf

--- Iac_Terraform_Jenkins_aws/vpc.tf
+++ Iac_Terraform_Jenkins_aws/vpc.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "app-vpc" {
 resource "aws_subnet" "jenkins-subnet-1" {
   vpc_id            = aws_vpc.app-vpc.id
   cidr_block        = var.subnet_cidr_block
-  availability_zone = var.availability_zone
+  availability_zone = "us-east-1c"
   map_public_ip_on_launch = true
   tags = {
     Name = "${var.env_prefix}-subnet-1"
