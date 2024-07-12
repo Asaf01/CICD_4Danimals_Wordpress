056397f16c79963d2dc3eb06164012972b35f433
Asaf01 <123545280+Asaf01@users.noreply.github.com>
2024-07-12 14:02:07 +0300

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -7,8 +7,8 @@ module "eks" {
 
     cluster_endpoint_public_access  = true
 
-    vpc_id = module.my-vpc.vpc_id
-    subnet_ids = module.my-vpc.private_subnets
+    vpc_id = module.app-vpc.vpc_id
+    subnet_ids = module.app-vpc.private_subnets
 
     tags = {
         environment = "development"
